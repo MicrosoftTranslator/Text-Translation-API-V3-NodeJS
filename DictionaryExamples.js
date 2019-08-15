09a76bcf430ad85e9537684db35bff5ab0ed504d
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2019-08-15 13:28:03 -0700

    Update DictionaryExamples.js

--- DictionaryExamples.js
+++ DictionaryExamples.js
@@ -6,23 +6,23 @@ way to make HTTP requests. */
 const request = require('request');
 const uuidv4 = require('uuid/v4');
 
-/* Checks to see if the subscription key is available
-as an environment variable. If you are setting your subscription key as a
-string, then comment these lines out.
-
-If you want to set your subscription key as a string, replace the value for
-the Ocp-Apim-Subscription-Key header as a string. */
-const subscriptionKey = process.env.YOUR_ENV_VARIABLE;
-if (!subscriptionKey) {
-  throw new Error('Environment variable for your subscription key is not set.')
-};
+var key_var = 'TRANSLATOR_TEXT_SUBSCRIPTION_KEY';
+if (!process.env[key_var]) {
+    throw new Error('Please set/export the following environment variable: ' + key_var);
+}
+var subscriptionKey = process.env[key_var];
+var endpoint_var = 'TRANSLATOR_TEXT_ENDPOINT';
+if (!process.env[endpoint_var]) {
+    throw new Error('Please set/export the following environment variable: ' + endpoint_var);
+}
+var endpoint = process.env[endpoint_var];
 
 /* If you encounter any issues with the base_url or path, make sure that you are
 using the latest endpoint: https://docs.microsoft.com/en-us/azure/cognitive-services/translator/reference/v3-0-dictionary-examples */
 function dictionaryExamples(){
     let options = {
         method: 'POST',
-        baseUrl: 'https://api.cognitive.microsofttranslator.com/',
+        baseUrl: endpoint,
         url: 'dictionary/examples',
         qs: {
           'api-version': '3.0',
